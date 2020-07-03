7a922279eba3debe69b09db1583ec38041f91b3b
hgopi <v-hgopi@carrentals.com>
2020-07-03 21:44:44 +0530

    commit chnages for folder structure

---
+++ build/precache-manifest.452f4f279079d79f3df050c08db134f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62300914c4cb482be150db03fc8e4926",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50f99a3ce6545fc4d38c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5d72316016a9ba2a8a86",
+    "url": "/static/js/2.21d239eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.21d239eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50f99a3ce6545fc4d38c",
+    "url": "/static/js/main.fbfd8bc9.chunk.js"
+  },
+  {
+    "revision": "bbc8f86fcc890a5888ad",
+    "url": "/static/js/runtime-main.b1f402ec.js"
+  }
+]);
