80fb2537374880a9efcd118743f8d0b80976f393
Sobin Anna Joseph <v-sannajoseph@carrentals.com>
2020-08-04 14:28:35 +0530

    short carousel Css fixed for mobile screen

--- src/components/Images.js
+++ src/components/Images.js
@@ -19,12 +19,12 @@ const Images = (props) => {
         },
         tablet: {
           breakpoint: { max: 1024, min: 464 },
-          items: 2,
+          items: 4,
           slidesToSlide: 2 // optional, default to 1.
         },
         mobile: {
           breakpoint: { max: 464, min: 0 },
-          items: 1,
+          items: 3,
           slidesToSlide: 1 // optional, default to 1.
         }
       };
@@ -41,8 +41,8 @@ const Images = (props) => {
         responsive={responsive}
         ssr={false} // means to render carousel on server-side.
         infinite={true}
-        autoPlay={false}
-        autoPlaySpeed={1000}
+        autoPlay={true}
+        autoPlaySpeed={3000}
         keyBoardControl={true}
         customTransition="all .5"
         transitionDuration={500}
