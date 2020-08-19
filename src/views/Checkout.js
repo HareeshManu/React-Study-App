dff95808162d22426d50835b25f2418e4806ad3d
Aivin Thomas <v-aithomas@carrentals.com>
2020-08-19 12:58:07 +0530

    outline

--- src/views/Checkout.js
+++ src/views/Checkout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/Checkout.scss';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 const Checkout = (props) => {
   return (
@@ -20,11 +20,21 @@ const Checkout = (props) => {
         </Col>
       </Row>
       <Row>
-        <Col xs={{ span: 6, offset: 1 }} md={{ span: 4, offset: 2}}>
+        <Col xs={{ span: 11, offset: 1 }} md={{ span: 4, offset: 2}} className="new-customer">
           <h2>New Customer</h2>
+          <p>Checkout Options</p>
+          <label for="register"> <input type="radio"  name="account" value="register" checked= "checked"></input><span class="register-account">Register account </span> </label>
+          <p>By creating an account you will be able to shop faster, be up to date on an order's status, and keep track of the orders you have previously made.</p>
+          <Button as="input" type="submit" value="Continue" id="button-account" />{' '}
         </Col>
-        <Col xs={{ span: 6, offset: 1 }} md={{ span: 3}}>
-          hoi
+        <Col xs={{ span: 11, offset: 1 }} md={{ span: 3}} className="new-customer">
+          <h2>Returning Customer</h2>
+          <label for="username"><span class="login-labels">Username</span></label><br/>
+          <input type="text" name="username" value="username"/><br/><br/>
+          <label for="password"><span class="login-labels">Password</span></label><br/>
+          <input type="password" name="password" value="password"/><br/><br/>
+          <Button as="input" type="submit" value="Login" id="button-signin" />{' '}<br/><br/>
+          <a href="#" target="_blank">Forgot your password</a>
         </Col>
       </Row>
       <Row>
